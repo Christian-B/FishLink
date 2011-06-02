72a555286e9bdef7603975b1180dc3a7559332b5
Christian-B <brenninc@cs.man.ac.uk>
2011-06-02 13:55:59 +0100

    Finished first version of MetaDataGraph writer.

--- src/uk/co/brenn/metadata/CYAB_Sheet.java
+++ src/uk/co/brenn/metadata/CYAB_Sheet.java
@@ -5,6 +5,7 @@
 
 package uk.co.brenn.metadata;
 
+import java.util.Date;
 import java.util.Iterator;
 import org.apache.poi.hssf.usermodel.DVConstraint;
 import org.apache.poi.hssf.usermodel.HSSFDataValidation;
@@ -12,6 +13,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataValidation;
 import org.apache.poi.ss.usermodel.DataValidationConstraint;
+import org.apache.poi.ss.usermodel.DataValidationHelper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellRangeAddressList;
@@ -51,6 +53,18 @@ public class CYAB_Sheet {
         return cell;
     }
 
+    private Cell getCellZeroBased(int column, int row){
+        Row poiRow = poiSheet.getRow(row);
+        if (poiRow == null){
+            poiRow = poiSheet.createRow(row);
+        }
+        Cell cell = poiRow.getCell(column);
+        if (cell == null){
+           cell = poiRow.createCell(column);
+        }
+        return cell;
+    }
+
     /**
      * Sets the value of the cell to the integer.
      * 
@@ -66,7 +80,35 @@ public class CYAB_Sheet {
     }
 
     /**
-     * Sets the value of the cell to the integer.
+     * Sets the value of the cell to the double.
+     *
+     * Creates the row and or cell if required.
+     *
+     * @param row Using Excel counting
+     * @param column Using Excel names
+     * @param value Integer value
+     */
+    void setValue (String column, int row, double value){
+        Cell cell = getCell(column, row);
+        cell.setCellValue(value);
+    }
+
+    /**
+     * Sets the value of the cell to the String.
+     *
+     * Creates the row and or cell if required.
+     *
+     * @param row Zero based
+     * @param column zero based
+     * @param value Integer value
+     */
+    public void setValueZeroBased (int column, int row, String value){
+        Cell cell = getCellZeroBased(column, row);
+        cell.setCellValue(value);
+    }
+
+    /**
+     * Sets the value of the cell to the String.
      *
      * Creates the row and or cell if required.
      *
@@ -75,6 +117,34 @@ public class CYAB_Sheet {
      * @param value Integer value
      */
     public void setValue (String column, int row, String value){
+        Cell cell = getCell(column, row);
+        cell.setCellValue(value);
+    }
+
+    /**
+     * Sets the value of the cell to the boolean.
+     *
+     * Creates the row and or cell if required.
+     *
+     * @param row Using Excel counting
+     * @param column Using Excel names
+     * @param value Integer value
+     */
+    public void setValue (String column, int row, boolean value){
+        Cell cell = getCell(column, row);
+        cell.setCellValue(value);
+    }
+
+    /**
+     * Sets the value of the cell to the Date.
+     *
+     * Creates the row and or cell if required.
+     *
+     * @param row Using Excel counting
+     * @param column Using Excel names
+     * @param value Integer value
+     */
+    public void setValue (String column, int row, Date value){
         Cell cell = getCell(column, row);
         cell.setCellValue(value);
     }
@@ -95,22 +165,30 @@ public class CYAB_Sheet {
        return "none";
    }
 
-    void addListValidation (String column, int row, String list, String popupTitle, String popupMessage)
+    void addValidation (String column, int row, String rule, String popupTitle, String popupMessage,
+            int errorStyle, String errorTitle, String errorMessage)
             throws JavaToExcelException {
         //ystem.out.println ("list at " + column + row);
+        System.out.println(column + ": " + rule);
         int columnNumber = POI_Utils.alphaToIndex(column);
         CellRangeAddressList addressList = new CellRangeAddressList(row - 1, row -1, columnNumber,  columnNumber);
+        DataValidationHelper dataValidationHelper = poiSheet.getDataValidationHelper();
         DataValidationConstraint constraint;
-        DataValidation dataValidation;
-        if (poiSheet instanceof HSSFSheet) {
-            constraint = DVConstraint.createFormulaListConstraint(list);
-            dataValidation = new HSSFDataValidation(addressList, constraint);
+        if (rule.isEmpty()){
+            constraint = dataValidationHelper.createCustomConstraint("true");
         } else {
-            throw new JavaToExcelException("Unable to add contraint to sheet of type " + poiSheet.getClass());
+            constraint = dataValidationHelper.createFormulaListConstraint(rule);
         }
+        DataValidation dataValidation = dataValidationHelper.createValidation(constraint, addressList);
         dataValidation.setSuppressDropDownArrow(false);
-        dataValidation.createPromptBox(popupTitle, popupMessage);
-        dataValidation.setShowPromptBox(true);
+        if (!popupTitle.isEmpty()){
+            dataValidation.createPromptBox(popupTitle, popupMessage);
+            dataValidation.setShowPromptBox(true);
+        }
+        dataValidation.setErrorStyle(errorStyle);
+        if (errorTitle.isEmpty()){
+            dataValidation.createErrorBox(errorTitle, errorMessage);
+        }
         poiSheet.addValidationData(dataValidation);
     }
 
--- src/uk/co/brenn/metadata/MetaDataCreator.java
+++ src/uk/co/brenn/metadata/MetaDataCreator.java
@@ -5,9 +5,21 @@
 
 package uk.co.brenn.metadata;
 
+import at.jku.xlwrap.common.XLWrapException;
+import at.jku.xlwrap.exec.ExecutionContext;
+import at.jku.xlwrap.spreadsheet.Cell;
+import at.jku.xlwrap.spreadsheet.Sheet;
+import at.jku.xlwrap.spreadsheet.TypeAnnotation;
+import at.jku.xlwrap.spreadsheet.Workbook;
+import at.jku.xlwrap.spreadsheet.XLWrapEOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.DataValidation;
+import org.apache.poi.ss.usermodel.Name;
 
 /**
  *
@@ -16,14 +28,18 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 public class MetaDataCreator {
 
     static private String MAIN_ROOT = "c:Dropbox/FishLink XLWrap data/";
+    static private String LIST_SHEET = "Lists";
 
+    private String metaMaster;
     private String metaRoot;
     private String dataRoot;
     
     static private int CATEGORY_ROW = 1;
     static private int FIELD_ROW = 2;
+    private Sheet Sheet;
 
-    public MetaDataCreator(String metaDir, String dataDir){
+    public MetaDataCreator(String metaMasterPath, String metaDir, String dataDir){
+        metaMaster = metaMasterPath;
         metaRoot = metaDir;
         dataRoot = dataDir;
     }
@@ -41,66 +57,190 @@ public class MetaDataCreator {
         metaWorkbook.write(metaRoot + fileFront + "MetaData.xls");
     }
 
-    private void prepareColumnA(CYAB_Sheet metaSheet, CYAB_Sheet dataSheet){
-        metaSheet.setValue("A", CATEGORY_ROW, "Category");
-        metaSheet.setValue("A", FIELD_ROW, "Field");
-        metaSheet.setValue("A", 3, "Id/Value Column (or \"Row\")");
-        metaSheet.setValue("A", 4, "'-- Links --");
-        metaSheet.setValue("A", 5, "Site");
-        metaSheet.setValue("A", 6, "*Site Link Type*");
-        metaSheet.setValue("A", 7, "Location");
-        metaSheet.setValue("A", 8, "Survey");
-        metaSheet.setValue("A", 9, "'-- Constants --");
-        metaSheet.setValue("A", 10, "Factor");
-        metaSheet.setValue("A", 11, "Unit");
-        metaSheet.setValue("A", 12, "DerivationFactor");
-        metaSheet.setValue("A", 13, "Date/Start Date");
-        metaSheet.setValue("A", 14, "");
-        metaSheet.setValue("A", 15, "End Date (if Applicable)");
-        int headerRow = 17;
-        metaSheet.setValue("A", headerRow, "Column in Data File ");
+    private void createNamedRanges (Workbook masterWorkbook, CYAB_Workbook metaWorkbook)
+            throws XLWrapException, XLWrapEOFException, JavaToExcelException{
+        Sheet masterSheet =  masterWorkbook.getSheet(LIST_SHEET);
+        CYAB_Sheet metaSheet = metaWorkbook.getSheet(LIST_SHEET);
+        int zeroColumn = 0;
+        String rangeName =  getTextZeroBased(masterSheet, zeroColumn, 0);
+        int categories = -1;
+        while (!rangeName.isEmpty()) {
+            String columnName = POI_Utils.indexToAlpha(zeroColumn);
+            metaSheet.setValueZeroBased(zeroColumn, 0, rangeName);
+            int zeroRow = 1;
+            String fieldName = getTextZeroBased(masterSheet, zeroColumn, zeroRow);
+            while (!fieldName.isEmpty()) {
+                metaSheet.setValueZeroBased(zeroColumn, zeroRow, fieldName);
+                zeroRow++;
+                fieldName = getTextZeroBased(masterSheet, zeroColumn, zeroRow);
+            } while (!fieldName.isEmpty());
+            Name range = metaWorkbook.createName();
+            range.setNameName(rangeName);
+            String rangeDef = "'" + LIST_SHEET + "'!$" + columnName + "$2:$" + columnName + "$" + zeroRow;
+            //=OFFSET(Sheet1!$A$1,0,0,COUNTA(Sheet1!$A:$A),1)
+            //String rangeDef = "OFFSET('" + LIST_SHEET + "'!$" + columnName + "$1,0,0,COUNTA('" + LIST_SHEET + "'!$" +
+            //        columnName + ":$" + columnName + ")-1,1)";
+            System.out.println (rangeName + " = " + rangeDef);
+            range.setRefersToFormula(rangeDef);
+            zeroColumn++;
+            rangeName = getTextZeroBased(masterSheet, zeroColumn, 0);
+            if (rangeName.isEmpty() && categories == -1){
+                //ystem.out.println("doing categories");
+                //found the space between Categories and the other nameRanges
+                categories = zeroColumn -1;
+                zeroColumn++;
+                rangeName = getTextZeroBased(masterSheet, zeroColumn, 0);
+            }
+        }
+        Name range = metaWorkbook.createName();
+        range.setNameName("Category");
+        String columnName = POI_Utils.indexToAlpha(categories);
+        range.setRefersToFormula("'" + LIST_SHEET + "'!$A1:$" + columnName + "$1");
+    }
+
+    private String getTextZeroBased(Sheet sheet, int column, int row) throws XLWrapException, XLWrapEOFException{
+        if (column >= sheet.getColumns()){
+            return "";
+        }
+        if (row >= sheet.getRows()){
+            return "";
+        }
+        Cell cell = sheet.getCell(column, row);
+        return cell.getText();
+    }
+
+    private int prepareColumnA(Sheet masterSheet, CYAB_Sheet metaSheet, Sheet dataSheet) throws XLWrapException, XLWrapEOFException{
+        int zeroRow = 0;
+        String value;
+        do {
+            value = getTextZeroBased(masterSheet, 0, zeroRow);
+            metaSheet.setValue("A",zeroRow + 1, value);
+            zeroRow++;
+            //ystem.out.println (value);
+        } while (!value.isEmpty());
         metaSheet.autoSizeColumn(0);
-        int maxRow = dataSheet.getLastRowNum();
-        System.out.println(maxRow);
+        int maxRow = dataSheet.getRows();
+        //ystem.out.println(maxRow);
         if (maxRow > 100){
             maxRow = 100;
         }
-        for (int row = 1; row <= maxRow; row++){
-           metaSheet.setValue("A",headerRow + row, row);
+        metaSheet.setValue("A",zeroRow + 1, "Column in Data File");
+        metaSheet.setValue("A",zeroRow + 2, "Header");
+        for (int row = 2; row <= maxRow; row++){
+           metaSheet.setValue("A",zeroRow + row + 1, row);
+        }
+        return zeroRow - 1;
+    }
+
+    private void prepareDropDowns(Sheet masterSheet, int lastMetaRow, CYAB_Sheet metaSheet, String column)
+            throws JavaToExcelException, XLWrapException, XLWrapEOFException{
+        for (int zeroRow = 0; zeroRow < lastMetaRow; zeroRow ++) {
+            String list = getTextZeroBased(masterSheet, 2, zeroRow);
+            //ystem.out.println(column + ":" + list);
+             String popupTitle = getTextZeroBased(masterSheet, 3, zeroRow);
+            String popupMessage =  getTextZeroBased(masterSheet, 4, zeroRow);
+            String errorStyleString =  getTextZeroBased(masterSheet, 5, zeroRow);
+            int errorStyle = DataValidation.ErrorStyle.STOP;
+            if (errorStyleString.startsWith("W")){
+                errorStyle = DataValidation.ErrorStyle.WARNING;
+            }
+            if (errorStyleString.startsWith("I")){
+                errorStyle = DataValidation.ErrorStyle.INFO;
+            }
+            String errorTitle =  getTextZeroBased(masterSheet, 6, zeroRow);
+            String errorMessage =  getTextZeroBased(masterSheet, 7, zeroRow);
+            metaSheet.addValidation(column, zeroRow + 1, list, popupTitle, popupMessage,
+                    errorStyle, errorTitle, errorMessage);
+       // metaSheet.addListValidation(column, FIELD_ROW,  "INDIRECT(SUBSTITUTE($" + column + "$1,\" \",\"_\"))",
+       //         "Type of feild", "Please select the feild that data in this column belongs to");
         }
     }
 
-    private void prepareDropDowns(CYAB_Sheet metaSheet, String column) throws JavaToExcelException{
-        metaSheet.addListValidation(column, CATEGORY_ROW, ListWriter.CATEGORY_NAME, "Type of data",
-                "Please select the catogory that data in this column belongs to");
-        metaSheet.addListValidation(column, FIELD_ROW,  "INDIRECT(SUBSTITUTE($" + column + "$1,\" \",\"_\"))",
-                "Type of feild", "Please select the feild that data in this column belongs to");
-    }
-
-    private void prepareSheet(CYAB_Sheet metaSheet, CYAB_Sheet dataSheet) throws JavaToExcelException{
-        prepareColumnA(metaSheet, dataSheet);
-        prepareDropDowns(metaSheet, "B");
-    }
-
-    private void prepareSheets(CYAB_Workbook metaWorkbook, CYAB_Workbook dataWorkbook) throws JavaToExcelException{
-        for (int sheetNumber = 0; sheetNumber < dataWorkbook.getNumberOfSheets(); sheetNumber++){
-            CYAB_Sheet dataSheet = dataWorkbook.getSheetAt(sheetNumber);
-            CYAB_Sheet metaSheet = metaWorkbook.getSheet(dataSheet.getSheetName());
-            prepareSheet(metaSheet, dataSheet);
+    private void copyData(int letterRow, CYAB_Sheet metaSheet, Sheet dataSheet, String column)
+            throws XLWrapException{
+        metaSheet.setValue(column, letterRow, column);
+        int maxRow = dataSheet.getRows();
+        if (maxRow > 100){
+            maxRow = 100;
+        }
+        int zeroColumn = POI_Utils.alphaToIndex(column) - 1; //-1 as Column A of Data goes in B of Meta
+        for (int zeroRow = 0; zeroRow < maxRow; zeroRow++){
+            //ystem.out.println(zeroRow);
+            try {
+                Cell cell = dataSheet.getCell(zeroColumn, zeroRow);
+                TypeAnnotation typeAnnotation = cell.getType();
+                switch (typeAnnotation){
+                    case BOOLEAN:
+                        boolean booleanValue = cell.getBoolean();
+                        //ystem.out.println(booleanValue);
+                        metaSheet.setValue(column, letterRow + zeroRow + 1, booleanValue);
+                        break;
+                    case NUMBER:
+                        double doubleValue = cell.getNumber();
+                        //ystem.out.println("number" + doubleValue);
+                        metaSheet.setValue(column, letterRow + zeroRow + 1, doubleValue);
+                        break;
+                    case TEXT:
+                        String textValue = cell.getText();
+                        //ystem.out.println(textValue);
+                        metaSheet.setValue(column, letterRow + zeroRow + 1, textValue);
+                        break;
+                    case DATE:
+                        Date dateValue = cell.getDate();
+                        //ystem.out.println(dateValue);
+                        metaSheet.setValue(column, letterRow + zeroRow + 1, dateValue);
+                        break;
+                    case NULL:
+                        //ystem.out.println("null");
+                        //Moving to copy
+                        break;
+                    default:
+                        throw new XLWrapException("Unexpected Cell Type");
+                }
+            } catch (XLWrapEOFException ex) {
+                //ystem.out.println("Exception");
+            }
         }
     }
 
-    public void prepareMetaData(String dataFile, String doi) throws FileNotFoundException, IOException, InvalidFormatException, JavaToExcelException{
-        CYAB_Workbook dataWorkbook = new CYAB_Workbook(dataRoot + dataFile);
+    private void prepareSheet(Sheet masterSheet, CYAB_Sheet metaSheet, Sheet dataSheet) 
+            throws JavaToExcelException, XLWrapException, XLWrapEOFException{
+        int lastMetaRow = prepareColumnA(masterSheet, metaSheet, dataSheet);
+        int lastColumn = dataSheet.getColumns();
+        for ( int zeroDataColumn = 0;  zeroDataColumn < lastColumn; zeroDataColumn++){
+            String column = POI_Utils.indexToAlpha(zeroDataColumn + 1); //Plus one as metaColumn on over from DetaColumn
+            prepareDropDowns(masterSheet, lastMetaRow, metaSheet, column);
+            copyData(lastMetaRow + 2, metaSheet, dataSheet, column);
+        }
+    }
+
+    private void prepareSheets(Workbook masterWorkbook, CYAB_Workbook metaWorkbook, Workbook dataWorkbook) 
+            throws JavaToExcelException, XLWrapException, XLWrapEOFException{
+        Sheet masterSheet = masterWorkbook.getSheet("Sheet1");
+        String[] dataSheets = dataWorkbook.getSheetNames();
+        for (int i = 0; i  < dataSheets.length; i++){
+            Sheet dataSheet = dataWorkbook.getSheet(dataSheets[i]);
+            CYAB_Sheet metaSheet = metaWorkbook.getSheet(dataSheets[i]);
+            prepareSheet(masterSheet, metaSheet, dataSheet);
+        }
+    }
+
+    public void prepareMetaData(ExecutionContext context, String dataFile, String doi) throws FileNotFoundException,
+            IOException, InvalidFormatException, JavaToExcelException, XLWrapException, XLWrapEOFException{
+        Workbook dataWorkbook = context.getWorkbook("file:" + dataRoot +dataFile);
+        Workbook masterWorkbook = context.getWorkbook("file:" + metaMaster);
         CYAB_Workbook metaWorkbook = new CYAB_Workbook();
         addMetaDataSheet(metaWorkbook, dataFile, doi);
-        ListWriter.writeLists(metaWorkbook);
-        prepareSheets(metaWorkbook,dataWorkbook);
+        createNamedRanges(masterWorkbook, metaWorkbook);
+        //ListWriter.writeLists(masterWorkbook, metaWorkbook);
+        prepareSheets(masterWorkbook, metaWorkbook,dataWorkbook);
         writeMeta(metaWorkbook, dataFile);
     }
 
-    public static void main(String[] args) throws IOException, FileNotFoundException, InvalidFormatException, JavaToExcelException{
-        MetaDataCreator creator = new MetaDataCreator(MAIN_ROOT + "Meta Data/",MAIN_ROOT + "Raw Data/");
-        creator.prepareMetaData ("Records.xls", "rec12564");
+    public static void main(String[] args) throws IOException, FileNotFoundException, InvalidFormatException, 
+            JavaToExcelException, XLWrapException, XLWrapEOFException{
+        MetaDataCreator creator = new MetaDataCreator(MAIN_ROOT + "metaMaster.xls", MAIN_ROOT + "Meta Data/",MAIN_ROOT + "Raw Data/");
+        ExecutionContext context = new ExecutionContext();
+        creator.prepareMetaData (context, "Records.xls", "rec12564");
     }
 }
