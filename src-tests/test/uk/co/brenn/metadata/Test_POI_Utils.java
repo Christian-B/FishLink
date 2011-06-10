5a4b71a461d1ef89b581970422d609151fbe1930
Christian-B <brenninc@cs.man.ac.uk>
2011-06-10 07:57:17 +0100

    Fixed a bug with column numbers and names above Y

---
+++ src-tests/test/uk/co/brenn/metadata/Test_POI_Utils.java
@@ -0,0 +1,33 @@
+
+package test.uk.co.brenn.metadata;
+
+import junit.framework.TestCase;
+import uk.co.brenn.metadata.POI_Utils;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Christian
+ */
+public class Test_POI_Utils extends TestCase{
+
+    
+    	@Test
+	public void testAlphaToIndexAndBack() {
+            for (int i = 0; i< 1000; i++){
+                String alpha = POI_Utils.indexToAlpha(i);
+                int col = POI_Utils.alphaToIndex(alpha);
+		assertEquals("AlphaToIndex and back failure",i, col);
+            }
+	}
+
+    	@Test
+	public void testSpecialCases() {
+           assertEquals("0 to A ","A", POI_Utils.indexToAlpha(0));
+           assertEquals("25 to Z ","Z", POI_Utils.indexToAlpha(25));
+           assertEquals("26 to AA ","AA", POI_Utils.indexToAlpha(26));
+           assertEquals("51 to AZ ","AZ", POI_Utils.indexToAlpha(51));
+           assertEquals("52 to BA ","BA", POI_Utils.indexToAlpha(52));
+	}
+}
--- src/uk/co/brenn/metadata/POI_Utils.java
+++ src/uk/co/brenn/metadata/POI_Utils.java
@@ -30,21 +30,26 @@ public class POI_Utils {
      */
     public static String indexToAlpha(int index){
         String reply = "";
-        if (index != 26){
+        //ystem.out.println(index);
+        if (index < 26){
            char first = (char)( index + 65);
            reply = first + "";
         } else {
            char last = (char)(index - ((index / 26) * 26) + 65);
-           String rest = indexToAlpha((index / 26));
+           String rest = indexToAlpha((index / 26)-1);
+           //ystem.out.println(rest + "-" + last);
            reply = rest + last;
         }
         if (index != alphaToIndex(reply)){
-            System.out.println("Error converting " + index);
-            System.out.println("Answer was "+ reply);
-            System.out.println("Which comes out as "+ alphaToIndex(reply));
-            int error = 1/0;
+            System.err.println("Error converting " + index);
+            System.err.println("Answer was "+ reply);
+            System.err.println("Which comes out as "+ alphaToIndex(reply));
+            throw new AssertionError("Error in indexToAlpha");
         }
         return reply;
     }
 
+    public static void main(String[] args) {
+        String test = indexToAlpha(48);
+    }
 }
