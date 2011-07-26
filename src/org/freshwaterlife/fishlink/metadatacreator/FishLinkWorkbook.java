f937a26a9416679b81bf0ff191e768aff798a767
Christian-B <brenninc@cs.man.ac.uk>
2011-07-26 16:15:47 +0100

    Java Docs

--- src/org/freshwaterlife/fishlink/metadatacreator/FishLinkWorkbook.java
+++ src/org/freshwaterlife/fishlink/metadatacreator/FishLinkWorkbook.java
@@ -3,8 +3,6 @@ package org.freshwaterlife.fishlink.metadatacreator;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Name;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -12,17 +10,28 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.freshwaterlife.fishlink.FishLinkException;
 
 /**
- *
+ * Wrapper around a Poi Workbook (
+ * 
+ * Currently uses HSSF but could be changed to XSSF without issue.
+ * Main role is to create FishLinkSheet, Create names and catch Exceptions in writing
  * @author Christian
  */
 public class FishLinkWorkbook {
 
-    Workbook poiWorkbook;
+    private Workbook poiWorkbook;
 
-    public FishLinkWorkbook(){
+    /**
+     * Creates a Workbook wrapping an Apache Poi HSSF workbook
+     */
+    FishLinkWorkbook(){
         poiWorkbook = new HSSFWorkbook();
     }
     
+    /**
+     * Gets or creates a Poi Sheet with this name and wraps it in a FISHLinkSheet.
+     * @param name Name of the workbook
+     * @return A Sheet with the given name.
+     */
     FishLinkSheet getSheet(String name) {
         Sheet sheet = poiWorkbook.getSheet(name);
         if (sheet == null) {
@@ -31,26 +40,13 @@ public class FishLinkWorkbook {
         return new FishLinkSheet(sheet);
     }
 
-    void write(String filePath) throws FishLinkException {
-        FileOutputStream fileOut;
-        try {
-            fileOut = new FileOutputStream(filePath);
-        } catch (IOException ex) {
-            throw new FishLinkException("Unable to open "+ filePath, ex);
-        }
-        try {
-            poiWorkbook.write(fileOut);
-        } catch (IOException ex) {
-            throw new FishLinkException("Unable to write "+ filePath, ex);
-        }
-        try {
-            fileOut.close();
-        } catch (IOException ex) {
-            throw new FishLinkException("Unable to close "+ filePath, ex);
-        }
-
-    }
-
+    /**
+     * Writes the workbook to file.
+     * 
+     * Will always be *.xls format regardless of the extention of the File.
+     * @param file A file with Ideally a *.xls Extension.
+     * @throws FishLinkException 
+     */
     void write(File file) throws FishLinkException {
         FileOutputStream fileOut;
         try {
@@ -71,6 +67,10 @@ public class FishLinkWorkbook {
 
     }
 
+    /**
+     * Create a blank Name (Excel Named Range)
+     * @return a blank Name (Excel Named Range)
+     */
     Name createName() {
         return poiWorkbook.createName();
     }
