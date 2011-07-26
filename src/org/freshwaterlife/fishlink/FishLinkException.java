f7151da8793419915e4120405c4237761345618e
Christian-B <brenninc@cs.man.ac.uk>
2011-07-26 08:34:07 +0100

    Documentation and spacing only

--- src/org/freshwaterlife/fishlink/FishLinkException.java
+++ src/org/freshwaterlife/fishlink/FishLinkException.java
@@ -1,21 +1,34 @@
 package org.freshwaterlife.fishlink;
 
-import at.jku.xlwrap.common.XLWrapException;
-
 /**
- *
- * @author Christian
+ * Catchall Exception thrown by any FishLink method that needs to throw an Exception.
+ * 
+ * @author Christian 
  */
 public class FishLinkException extends Exception {
 
     /**
      * Constructs an instance of <code>FishLinkException</code> with the specified detail message.
-     * @param msg the detail message.
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
      */
     public FishLinkException(String msg) {
         super(msg);
     }
 
+   /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * <code>cause</code> is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
     public FishLinkException(String msg, Exception ex) {
         super(msg, ex);
     }
--- src/org/freshwaterlife/fishlink/FishLinkPaths.java
+++ src/org/freshwaterlife/fishlink/FishLinkPaths.java
@@ -1,36 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.freshwaterlife.fishlink;
 
 /**
- *
+ * Centralization point for all paths hard coded in Fish.Link
+ * 
  * @author christian
  */
 public class FishLinkPaths {
 
-   public static final String MAP_FILE_ROOT = "output/mappings/";
+    /**
+     * Directory to which the mapping files are written
+     */
+    public static final String MAP_FILE_ROOT = "output/mappings/";
 
-   public static final String RDF_FILE_ROOT = "output/rdf/";
+    /**
+     * Directory to which the RDF output files are written
+     */
+    public static final String RDF_FILE_ROOT = "output/rdf/";
 
- //  public static final String META_FILE_ROOT = "output/meta/";
+    /**
+     * Base URL to be used in the RDF 
+     */
+    public static final String RDF_BASE_URL = "http://rdf.freshwaterlife.org/";
 
-   public static final String RDF_BASE_URL = "http://rdf.freshwaterlife.org/";
-
-   public static final String MAIN_ROOT = "c:/Dropbox/FishLink XLWrap data/";
-
-   public static final String META_FILE_ROOT = MAIN_ROOT + "Meta Data/";
+    /**
+     * Directory to which the MetaData files created will be written.
+     */
+    public static final String META_FILE_ROOT = "c:/Dropbox/FishLink XLWrap data/Meta Data/";
    
-   //Old Meta data is optional - will not throw an error if missing
- //  public static final String OLD_META_DIR = MAIN_ROOT + "Old Meta Data/";
-   
-//   public static final String RAW_DIR = MAIN_ROOT + "Raw Data/";
-
-   //The file: bit is required by xlwrap whiuch can alos handle http urls.
-//   static public final String  DROPBOX = "file:c:/Dropbox/FishLink XLWrap data/";
-
-   static public final String MASTER_FILE = "data/MetaMaster.xlsx";
+    /**
+     * Hard coded in location of the MetaMaster
+     */
+    static public final String MASTER_FILE = "data/MetaMaster.xlsx";
 
 }
