9f567b406ca6729fb7ce7b5f56a3b95f04d6f1a4
Christian-B <brenninc@cs.man.ac.uk>
2011-08-01 16:30:50 +0100

    javadocs

--- src/org/freshwaterlife/fishlink/xlwrap/WorkbookWrite.java
+++ src/org/freshwaterlife/fishlink/xlwrap/WorkbookWrite.java
@@ -14,23 +14,39 @@ import org.freshwaterlife.fishlink.FishLinkPaths;
 import org.freshwaterlife.fishlink.FishLinkUtils;
 
 /**
- *
+ * Write the mapping file for a whole workbook.
+ * 
+ * Creates the Workbook and the classes based on the MetaMaster.
+ * Does the workbook level writing and then calls the Sheets to write themselves.
+ * 
  * @author Christian
  */
 public class WorkbookWrite {
 
+    /**
+     * Workbook PID used to create a unique map file name
+     */
     private String pid;
 
+    /**
+     * The individual sheet (wrappers) which do most of the actually writing.
+     */
     private SheetWrite[] sheetWrites;
 
+    /**
+     * Constructor of the workbook wrapper as well as the wrappers around the sheets in this workbook and the MetaMaster. 
+     * 
+     * Also checks the data workbook against the MetaMaster.
+     * 
+     * @param dataUrl URL (in xlwrap format) to the Workbook that holds the Annotated Data
+     * @param pid Unique identifier to this data. Used in URIs and file naming.
+     * @param masterUrl URL (in xlwrap format) to the MetaMaster Workbook
+     * @throws FishLinkException 
+     */
     public WorkbookWrite(String dataUrl, String pid, String masterUrl) throws FishLinkException{
         ExecutionContext context = new ExecutionContext();
-        Workbook annotatedWorkbook;
-        try {
-            annotatedWorkbook = context.getWorkbook(dataUrl);
-        } catch (XLWrapException ex) {
-            throw new FishLinkException("Error opening the workbook " + dataUrl, ex);
-        }
+        this.pid = pid;
+        //Wrap the MetaMaster
         Sheet masterListSheet;
         try {
             masterListSheet = context.getSheet(masterUrl, FishLinkConstants.LIST_SHEET);
@@ -38,6 +54,7 @@ public class WorkbookWrite {
             throw new FishLinkException("Error opening the vocabulary sheet " + FishLinkConstants.LIST_SHEET + 
                     " in ExcelSheet " + masterUrl, ex);
         }
+        NameChecker nameChecker = new NameChecker(masterListSheet);
         Sheet masterDropdownSheet;
         try {
             masterDropdownSheet = context.getSheet(masterUrl, FishLinkConstants.DROP_DOWN_SHEET);
@@ -45,9 +62,14 @@ public class WorkbookWrite {
             throw new FishLinkException("Error opening the dropdown sheet " + FishLinkConstants.DROP_DOWN_SHEET+ 
                     " in ExcelSheet " + masterUrl, ex);
         }
-        NameChecker nameChecker = new NameChecker(masterListSheet);
         MasterReader masterReader = new MasterReader(masterDropdownSheet);
-        this.pid = pid;
+        //Wrap the workbook
+        Workbook annotatedWorkbook;
+        try {
+            annotatedWorkbook = context.getWorkbook(dataUrl);
+        } catch (XLWrapException ex) {
+            throw new FishLinkException("Error opening the workbook " + dataUrl, ex);
+        }
         String[] sheetNames = annotatedWorkbook.getSheetNames();
         sheetWrites = new SheetWrite[sheetNames.length - 1];
         int j = 0;
@@ -61,7 +83,6 @@ public class WorkbookWrite {
             if (sheet.getName().equals("MetaData") || sheet.getName().equals("Lists")) {
                 //do nothing
             } else {
-                //ystem.out.println("  " + i + " " + sheetNames.length);
                 sheetWrites[j] = new SheetWrite(nameChecker, sheet, i, dataUrl, pid );
                 masterReader.check(sheetWrites[j]);
                 j++;
@@ -69,31 +90,11 @@ public class WorkbookWrite {
         }
     }
 
-    /*public WorkbookWrite (String metaFileName) throws FishLinkException{
-        Workbook metaWorkbook;
-        Sheet metaData;
-        metaWorkbook = FishLinkUtils.getWorkbook("file:" + metaRoot + metaFileName);
-        metaData = FishLinkUtils.getSheet(metaWorkbook, "MetaData");
-        Cell cell = FishLinkUtils.getCell (metaData, 1, 0);
-        String dataFileName = FishLinkUtils.getText(cell);
-        Workbook dataWorkbook = FishLinkUtils.getWorkbook("file:" + dataRoot + dataFileName);
-        cell = FishLinkUtils.getCell(metaData, 1, 1);
-        doi = FishLinkUtils.getText(cell);
-        String[] sheetNames = metaWorkbook.getSheetNames();
-        sheetWrites = new SheetWrite[sheetNames.length - 2];
-        int j = 0;
-        for (int i = 0; i< sheetNames.length; i++ ){
-            if (sheetNames[i].equals("MetaData") || sheetNames[i].equals("Lists")) {
-                //do nothing
-            } else {
-                sheetWrites[j] = new SheetWrite(metaWorkbook, "file:" + dataRoot + dataFileName, doi, sheetNames[i]);
-                MasterReader masterReader = new MasterReader ();
-                masterReader.check(sheetWrites[j]);
-                j++;
-            }
-        }
-    }*/
-
+    /**
+     * Writes the data independent part of the mapping file.
+     * @param writer
+     * @throws FishLinkException 
+     */
     private void writePrefix (BufferedWriter writer) throws FishLinkException {
         try {
             writer.write("@prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#> .");
@@ -128,6 +129,14 @@ public class WorkbookWrite {
         }
     }
 
+    /**
+     * Writes the mapping file.
+     * 
+     * Creates a mapping file based on the PID.
+     * Writes the prefix and then asks each wrapped sheet to write its part of the mapping file.
+     * @return The Mapping file.
+     * @throws FishLinkException 
+     */
      public File writeMap() throws FishLinkException {
         FishLinkUtils.report("write map");
         File mapFile = new File(FishLinkPaths.MAP_FILE_ROOT);
